7ca7c6fdb2f0b4bbabf2bd91fa4eee62966be618
djsulli24 <djsulli24@gmail.com>
2017-12-15 16:02:11 -0600

    Edit field now has autocomplete functionality, too.

--- js/components/addresssave.js
+++ js/components/addresssave.js
@@ -91,8 +91,13 @@ postal_code: 'short_name'
 function initAutocomplete() {
 // Create the autocomplete object, restricting the search to geographical
 // location types.
+
 autocomplete = new google.maps.places.Autocomplete(
     /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+    {types: ['geocode']});
+
+autocomplete2 = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('autocompleteEdit')),
     {types: ['geocode']});
 
 // When the user selects an address from the dropdown, populate the address
