c814f481f178fa17c47d4aaa40235d42b27ba6d2
Ryan Lee <marble92@sbcglobal.net>
2017-12-14 19:25:41 -0600

    nearby.js updated to console log all relevant information

--- js/components/nearby.js
+++ js/components/nearby.js
@@ -1,11 +1,30 @@
 // Zomato API 
-var queryURL = "https://developers.zomato.com/api/v2.1/geocode?lat=30.266748&lon=-97.74176"
+var latitude = 30.266748;
+var longitude = -97.74176;
+var nearbyURL = "https://developers.zomato.com/api/v2.1/geocode?lat=" + latitude + "&lon=" + longitude;
 
 $.ajax({
-	url: queryURL,
+	url: nearbyURL,
 	method: 'GET',
 	"headers": {
     "user-key": "3db1ea2472489650b0b491a357916958"}
 }).done(function(response) {
-	console.log(response);
+
+	for (i = 0; i < 5; i++){
+
+		console.log(response.nearby_restaurants[i].restaurant);
+		console.log(response.nearby_restaurants[i].restaurant.name);
+
+		// Console Log Address Output
+		console.log(response.nearby_restaurants[i].restaurant.location.address);
+		console.log(response.nearby_restaurants[i].restaurant.location.latitude);
+		console.log(response.nearby_restaurants[i].restaurant.location.longitude);
+
+		// Console Log Restaurant Information
+		console.log(response.nearby_restaurants[i].restaurant.cuisines);
+		console.log(response.nearby_restaurants[i].restaurant.price_range);
+		console.log(response.nearby_restaurants[i].restaurant.user_rating.aggregate_rating);
+		console.log(response.nearby_restaurants[i].restaurant.thumb);
+	}
+
 });
