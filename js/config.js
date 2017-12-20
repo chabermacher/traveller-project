c6602588ca8962442ebee790c3feaafd133224e2
Cecil Habermacher <29548074+chabermacher@users.noreply.github.com>
2017-12-19 18:12:23 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCkncjVkFvOunt8hcaWlDIMx9f1XbBLYIQ",
+  authDomain: "traveler-project-ca386.firebaseapp.com",
+  databaseURL: "https://traveler-project-ca386.firebaseio.com",
+  projectId: "traveler-project-ca386",
+  storageBucket: "traveler-project-ca386.appspot.com",
+  messagingSenderId: "932064209486"
+};
+const GOOGLE_MAPS_KEY = 'AIzaSyCiHLoQIZp6HoFeFNzCOzrOm2H3sM3NXAo';
