f21b46f20a2c474b3c42c577905107f4edf506d2
Philip Harrell <alightedlamp@gmail.com>
2017-12-20 22:51:50 -0600

    Clean up app structure

--- js/app.js
+++ js/app.js
@@ -129,6 +129,30 @@ firebase.auth().onAuthStateChanged(function(user) {
   });
 });
 
+const getWeather = function() {
+  var APIKey = '166a433c57516f51dfab1f7edaed8413';
+
+  // Here we are building the URL we need to query the database
+  var queryURL =
+    'https://api.openweathermap.org/data/2.5/weather?' +
+    'q=austin&units=imperial&appid=' +
+    APIKey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).done(function(response) {
+    // Transfer content to HTML
+    $('#weather').html(
+      `${response.name} ${Math.round(response.main.temp)} &deg; F, ${
+        response.weather[0].description
+      }`
+    );
+    $('#weather').css('textTransform', 'capitalize');
+  });
+};
+
 function init() {
   const autocomplete = new google.maps.places.Autocomplete(
     /** @type {!HTMLInputElement} */ (document.getElementById('autocomplete')),
@@ -141,4 +165,6 @@ function init() {
     )),
     { types: ['geocode'] }
   );
+
+  getWeather();
 }
